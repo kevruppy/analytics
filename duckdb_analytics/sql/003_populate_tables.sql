d90fe242b416f31b01b6fb9a2b75f2df8c3e079b
KR <mail@kevin-ruppert.de>
2024-07-21 23:10:13 +0000

    populate base tables

--- duckdb_analytics/sql/003_populate_tables.sql
+++ duckdb_analytics/sql/003_populate_tables.sql
@@ -1,1 +1,26 @@
-SELECT 1 AS X;
+-- ORDERS
+
+-- LOAD CSV
+
+CREATE OR REPLACE TABLE TMP_ORDERS AS
+SELECT * FROM READ_CSV('/workspaces/analytics/sample_data/orders.csv'
+,all_varchar = True
+,header = TRUE
+,sep = ',');
+
+-- UNLOAD THE DATA AS JSON
+
+COPY TMP_ORDERS TO 'TMP_ORDERS.json' (FORMAT JSON);
+
+-- RE-LOAD THE JSON
+--> ONLY JSON IN RAW_DATA SCHEMA
+
+-- PROVISION RULES
+
+INSERT INTO RAW_DATA.PROVISION_RULES (LOAD_RESULT)
+SELECT JSON AS LOAD_RESULT --noqa: RF04
+FROM
+	READ_JSON(
+		'/workspaces/analytics/sample_data/provision_rules.json'
+		,RECORDS = FALSE
+	);

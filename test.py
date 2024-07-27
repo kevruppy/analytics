5cb9884664d6e6bb85692868904ac5d217d1383e
KR <mail@kevin-ruppert.de>
2024-07-27 19:05:01 +0000

    more formatting

--- test.py
+++ test.py
@@ -1,20 +1,35 @@
-import yaml
+import sys
 from pathlib import Path
 
-ROOT_LEVEL_ORDER = ['version', 'models']
-MODEL_PROPERTIES_ORDER = ['name', 'description', 'config', 'data_tests', 'columns']
-MODEL_CONFIG_ORDER = ['enabled', 'schema', 'materialized']
-COLUMNS_CONFIG_ORDER = ['name', 'description', 'data_type', 'data_tests']
-OPTIONAL_COLUMNS_CONFIG = ['data_tests']
+import yaml
 
-file_path = Path('/workspaces/analytics/dbt_analytics/models/stage/_stg_orders.yml')
-with file_path.open('r') as file:
+ROOT_LEVEL_ORDER = ["version", "models"]
+MODEL_PROPERTIES_ORDER = ["name", "description", "config", "data_tests", "columns"]
+MODEL_CONFIG_ORDER = ["enabled", "schema", "materialized"]
+COLUMNS_CONFIG_ORDER = ["name", "description", "data_type", "data_tests"]
+OPTIONAL_COLUMNS_CONFIG = ["data_tests"]
+
+try:
+    file_path = Path(sys.argv[1])
+except IndexError:
+    print(
+        "Error: No file path provided. Please provide the path to a YAML file to be validated"
+    )
+    sys.exit(1)
+
+if not file_path.is_file():
+    print(f"Error: The provided path '{file_path}' does not point to an existing YAML")
+    sys.exit(1)
+
+with file_path.open("r") as file:
     cfg = yaml.safe_load(file)
 
 root_level_order = list(cfg.keys())
 model_properties_order = list(cfg.get("models", [{}])[0].keys())
 model_config_order = list(cfg.get("models", [{}])[0].get("config", {}).keys())
-columns_config_orders = [list(col.keys()) for col in cfg.get("models", [{}])[0].get("columns", [])]
+columns_config_orders = [
+    list(col.keys()) for col in cfg.get("models", [{}])[0].get("columns", [])
+]
 
 if ROOT_LEVEL_ORDER == root_level_order:
     print("Root level correctly sorted")
@@ -35,8 +50,12 @@ for i, col_order in enumerate(columns_config_orders):
     if COLUMNS_CONFIG_ORDER == col_order:
         print(f"Column config [{i}] correctly sorted")
     else:
-        expected_without_optional = [item for item in COLUMNS_CONFIG_ORDER if item not in OPTIONAL_COLUMNS_CONFIG]
+        expected_without_optional = [
+            item for item in COLUMNS_CONFIG_ORDER if item not in OPTIONAL_COLUMNS_CONFIG
+        ]
         if expected_without_optional == col_order:
-            print(f"Column config [{i}] correctly sorted (excluding optional property 'data_tests')")
+            print(
+                f'Column config [{i}] correctly sorted (excluding optional property "data_tests")'
+            )
         else:
             print(f"Column config [{i}] not correctly sorted")

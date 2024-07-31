ea0bb17f3d6c70cff9beb9028d50724b0ba7540e
KR <mail@kevin-ruppert.de>
2024-07-31 09:44:46 +0000

    -

--- dbt_analytics/models/stage/stg_net_promotor_scores.sql
+++ dbt_analytics/models/stage/stg_net_promotor_scores.sql
@@ -1,11 +1,20 @@
+WITH CTE AS (
+	SELECT
+		TRANSACTION_ID
+		,RATING_DATE
+		,RATING
+		,TOOL
+	FROM
+		{{ source('raw', 'net_promotor_scores') }}
+	{% if is_incremental() %}
+		WHERE
+			TRANSACTION_ID NOT IN (SELECT DISTINCT TRANSACTION_ID FROM {{ this }})
+	{% endif %}
+)
+
 SELECT
 	TRANSACTION_ID
 	,RATING_DATE
 	,RATING
 	,TOOL
-FROM
-	{{ source('raw', 'net_promotor_scores') }}
-{% if is_incremental() %}
-	WHERE
-		TRANSACTION_ID NOT IN (SELECT DISTINCT TRANSACTION_ID FROM {{ this }})
-{% endif %}
+FROM CTE
--- dbt_analytics/models/stage/stg_orders_with_placement.sql
+++ dbt_analytics/models/stage/stg_orders_with_placement.sql
@@ -1,7 +1,11 @@
-SELECT (LOAD_RESULT ->> 'ORDER_ID')::INTEGER AS ORDER_ID
-FROM
-	{{ source('raw', 'orders_with_placement') }}
-{% if is_incremental() %}
-	WHERE
-		(LOAD_RESULT ->> 'ORDER_ID')::INTEGER NOT IN (SELECT DISTINCT ORDER_ID FROM {{ this }})
-{% endif %}
+WITH CTE AS (
+	SELECT (LOAD_RESULT ->> 'ORDER_ID')::INTEGER AS ORDER_ID
+	FROM
+		{{ source('raw', 'orders_with_placement') }}
+	{% if is_incremental() %}
+		WHERE
+			(LOAD_RESULT ->> 'ORDER_ID')::INTEGER NOT IN (SELECT DISTINCT ORDER_ID FROM {{ this }})
+	{% endif %}
+)
+
+SELECT ORDER_ID FROM CTE

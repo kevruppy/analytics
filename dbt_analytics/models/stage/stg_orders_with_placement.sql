8f1efdae38a6addd9efd115dd9b2ee5572f90d03
KR <mail@kevin-ruppert.de>
2024-07-31 09:17:25 +0000

    -

--- dbt_analytics/models/stage/stg_orders_with_placement.sql
+++ dbt_analytics/models/stage/stg_orders_with_placement.sql
@@ -1,7 +1,7 @@
-SELECT ORDER_ID
+SELECT (LOAD_RESULT ->> 'ORDER_ID')::INTEGER AS ORDER_ID
 FROM
 	{{ source('raw', 'orders_with_placement') }}
 {% if is_incremental() %}
 	WHERE
-		ORDER_ID NOT IN (SELECT DISTINCT ORDER_ID FROM {{ this }})
+		(LOAD_RESULT ->> 'ORDER_ID')::INTEGER NOT IN (SELECT DISTINCT ORDER_ID FROM {{ this }})
 {% endif %}

ef108a0cd161ace1730660a98b82fddaa199caa1
KR <mail@kevin-ruppert.de>
2024-08-01 18:35:23 +0000

    -

--- dbt_analytics/models/intermediate/int_orders_proportional_provisions.sql
+++ dbt_analytics/models/intermediate/int_orders_proportional_provisions.sql
@@ -1,13 +1,14 @@
 WITH CTE AS (
-    SELECT
-        ord.* 
-    FROM {{ ref('int_orders_gross_net_agg_per_month') }} AS ORD
-INNER JOIN
-	{{ ref('cre_provision_rules_proportional') }} AS PROV
-ON
-	ORD.PRODUCT_NAME = PROV.PRODUCT_NAME
-AND
-	ORD.CREATION_MONTH BETWEEN PROV.START_DATE AND PROV.END_DATE
-and 
-cnt_ORDERS > PROV.PROPORTIONAL_PROVISION_TARGET_VALUE
+	SELECT ORD.*
+	FROM {{ ref('int_orders_gross_net_agg_per_month') }} AS ORD
+	INNER JOIN
+		{{ ref('cre_provision_rules_proportional') }} AS PROV
+		ON
+			ORD.PRODUCT_NAME = PROV.PRODUCT_NAME
+			AND
+			ORD.CREATION_MONTH BETWEEN PROV.START_DATE AND PROV.END_DATE
+			AND
+			ORD.CNT_ORDERS > PROV.PROPORTIONAL_PROVISION_TARGET_VALUE
 )
+
+SELECT * FROM CTE

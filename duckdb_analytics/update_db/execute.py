c9c7a4399745f346ead2d2628ee68a33d38d0f3a
KR <mail@kevin-ruppert.de>
2024-08-16 10:42:23 +0000

    -

--- duckdb_analytics/update_db/execute.py
+++ duckdb_analytics/update_db/execute.py
@@ -10,9 +10,9 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-print(os.getcwd())
-"""
-with open("/sample_data/nps.json", "r") as f:
+with open(
+    "/workspaces/analytics/duckdb_analytics/update_db/sample_data/nps.json", "r"
+) as f:
     file = json.load(f)
 
 df = pd.DataFrame.from_dict(file, dtype=str)
@@ -23,4 +23,3 @@ with duckdb.connect(os.getenv("DB_PATH")) as con:
     con.execute(
         "INSERT INTO ANALYTICS.RAW_DATA.NET_PROMOTOR_SCORES (TRANSACTION_ID, RATING_DATE, RATING, TOOL) SELECT COLUMNS(*) FROM df"
     )
-"""

42863535d10c2142d6e2c7acaa6c39a025550830
KR <mail@kevin-ruppert.de>
2024-08-27 21:19:14 +0000

    -

--- get_exchange_rates.py
+++ get_exchange_rates.py
@@ -1,9 +1,38 @@
+## check for status code/ retries etc.
+
 import requests
 
-url = "https://api.frankfurter.app/2023-12-31?amount=10&from=GBP&to=USD"
+DATES = [
+    "2022-01-31",
+    "2022-02-28",
+    "2022-03-31",
+    "2022-04-30",
+    "2022-05-31",
+    "2022-06-30",
+    "2022-07-31",
+    "2022-08-31",
+    "2022-09-30",
+    "2022-10-31",
+    "2022-11-30",
+    "2022-12-31",
+    "2023-01-31",
+    "2023-02-28",
+    "2023-03-31",
+    "2023-04-30",
+    "2023-05-31",
+    "2023-06-30",
+    "2023-07-31",
+    "2023-08-31",
+    "2023-09-30",
+    "2023-10-31",
+    "2023-11-30",
+    "2023-12-31",
+]
 
-# end of month reporting (24 requests -> no throttling, eur->usd)
+res = []
+for date in DATES:
+    url = f"https://api.frankfurter.app/{date}"
+    response = requests.get(url=url, params={"amount": 1, "from": "EUR", "to": "USD"})
+    res.append(response.json())
 
-response = requests.get(url=url, params={"amount": 1, "from": "EUR", "to": "USD"})
-
-print(response.text)
+print(res)

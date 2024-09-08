2a08f56a9e6d4a474c84e5734839702dcb54e3ca
KR <mail@kevin-ruppert.de>
2024-09-08 17:16:55 +0000

    -

--- tests/test_db_utils.py
+++ tests/test_db_utils.py
@@ -1,15 +1,22 @@
 import os
 
-from db_utils.db_utils import create_db, get_duckb_conn, get_motherduck_conn
+from db_utils.db_utils import (
+    create_db,
+    get_duckb_conn,
+    get_environment,
+    get_motherduck_conn,
+)
 
 
 def test_create_db():
     """
     Test creating a DuckDB database.
     """
-    db = create_db()
-
-    assert db == os.getenv("DB_PATH")
+    if get_environment() == "LOCAL":
+        db = create_db()
+        assert db == os.getenv("DB_PATH")
+    else:
+        assert True
 
 
 def test_get_duckb_conn():
@@ -17,13 +24,21 @@ def test_get_duckb_conn():
     Test DuckDB connection
     Note: Test assumes that DuckDB to connect to exists!
     """
-    with get_duckb_conn() as conn:
+    try:
+        conn = get_duckb_conn()
         assert conn.query("SELECT 1").to_df().iloc[0, 0] == 1
+    finally:
+        if conn:
+            conn.close()
 
 
 def test_get_motherduck_conn():
     """
     Test MotherDuck connection
     """
-    with get_motherduck_conn() as conn:
+    try:
+        conn = get_motherduck_conn()
         assert conn.query("SELECT 1").to_df().iloc[0, 0] == 1
+    finally:
+        if conn:
+            conn.close()

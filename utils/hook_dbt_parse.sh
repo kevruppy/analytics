e68c3b6b0654f834974ebcc39d3f991634107531
KR <mail@kevin-ruppert.de>
2024-09-08 10:46:32 +0000

    -

--- utils/hook_dbt_parse.sh
+++ utils/hook_dbt_parse.sh
@@ -4,3 +4,4 @@ source /workspaces/analytics/.venv/bin/activate
 cd /workspaces/analytics/dbt_analytics
 dbt parse --project-dir /workspaces/analytics/dbt_analytics
 unset DB_PATH
+echo "Finished creating manifest.json (dbt)"

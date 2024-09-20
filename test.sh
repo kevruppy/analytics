af3a1bb6f0dffef56b1a70394bad0163a0c71c43
KR <mail@kevin-ruppert.de>
2024-09-20 17:57:07 +0000

    -

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-py_files=$(git diff origin/main HEAD --name-only | grep '.py$' | xargs)
+py_files=$(git rev-parse --show-toplevel && git diff origin/main HEAD --name-only | grep '.py$' | xargs)
 pylint "$py_files"

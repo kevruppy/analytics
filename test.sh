1a3064f547e499f480a2420549213bb0cf216dbf
KR <mail@kevin-ruppert.de>
2024-09-20 09:09:02 +0000

    -

--- test.sh
+++ test.sh
@@ -1,7 +1,1 @@
-files=$(git diff --name-only --cached | grep '.py$')
-echo "Found files:$files"
-if [[ -n "$files" ]]; then
-    echo "HI"
-else
-    echo "No .py files to be linted found. Skipping Pylint."
-fi
+echo "py_files=$(git diff --name-only --cached | grep '.py$')"

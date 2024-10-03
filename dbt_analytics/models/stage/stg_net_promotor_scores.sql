65772c03ccc467cfc315e5b42018267bc77c2827
KR <mail@kevin-ruppert.de>
2024-10-03 18:11:32 +0000

    -

--- dbt_analytics/models/stage/stg_net_promotor_scores.sql
+++ dbt_analytics/models/stage/stg_net_promotor_scores.sql
@@ -8,7 +8,7 @@ WITH CTE AS (
 		{{ source('raw', 'net_promotor_scores') }}
 	{% if is_incremental() %}
 		WHERE
-			TRANSACTION_ID NOT IN (SELECT DISTINCT TRANSACTION_ID FROM {{ this }})
+			TRANSACTION_ID NOT IN (SELECT DISTINCT TGT.TRANSACTION_ID FROM {{ this }} AS TGT)
 	{% endif %}
 )
 
